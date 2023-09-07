533453732938d7493491d65d35e513554c7806d7
NnDd <143475900+Nn-Dd@users.noreply.github.com>
2023-09-06 20:53:49 -0600

    Added all the game code

---
+++ javascript.js
@@ -0,0 +1,53 @@
+let playerChoice = "";
+let winner = document.getElementById("winner-span");
+
+function getComputerChoice() {
+  const threeChoices = ["Rock", "Paper", "Scissors"];
+  return threeChoices[Math.floor(Math.random() * threeChoices.length)];
+}
+
+function getPlayerChoiceHand() {
+  playerChoice = "Hand";
+  const computerChoice = getComputerChoice();
+  game(playerChoice, computerChoice);
+}
+
+function getPlayerChoiceRock() {
+  playerChoice = "Rock";
+  const computerChoice = getComputerChoice();
+  game(playerChoice, computerChoice);
+}
+
+function getPlayerChoiceScissors() {
+  playerChoice = "Scissors";
+  const computerChoice = getComputerChoice();
+  game(playerChoice, computerChoice);
+}
+
+function game(playerChoice, computerChoice) {
+  console.log("Player Choice: ", playerChoice);
+  console.log("Computer Choice: ", computerChoice);
+
+  switch (true) {
+    case playerChoice === computerChoice:
+      winner.innerText = "It's a draw!!!";
+      document.getElementById("computerSelection-span").innerHTML =
+        computerChoice;
+      document.getElementById("playerSelection-span").innerHTML = playerChoice;
+      break;
+    case (playerChoice === "Rock" && computerChoice === "Scissors") ||
+      (playerChoice === "Paper" && computerChoice === "Rock") ||
+      (playerChoice === "Scissors" && computerChoice === "Paper"):
+      winner.innerText = "Player wins!";
+      document.getElementById("computerSelection-span").innerHTML =
+        computerChoice;
+      document.getElementById("playerSelection-span").innerHTML = playerChoice;
+      break;
+    default:
+      winner.innerText = "Computer wins!";
+      document.getElementById("computerSelection-span").innerHTML =
+        computerChoice;
+      document.getElementById("playerSelection-span").innerHTML = playerChoice;
+      break;
+  }
+}
